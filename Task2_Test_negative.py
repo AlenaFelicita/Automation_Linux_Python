55ab8530af6e2a8cc587413427d263b6687f2183
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2023-12-06 12:27:28 +0300

    Create Task2_Test_negative.py
    Урок 2. Создание первых тестов на pytest

---
+++ Task2_Test_negative.py
@@ -0,0 +1,11 @@
+import subprocess
+out = "/home/user/out"
+folder1 = "/home/user/folder1"
+
+
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+    if (text in result.stdout or text in result.stderr) and result.returncode != 0:
+        return True
+    else:
+        return False
