4fd31be381e20b703ca431c35cd882029e07ace2
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2023-12-06 12:50:53 +0300

    Create Task4_Ex.py

---
+++ Task4_Ex.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+def checkout_negative(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+    if (text in result.stdout or text in result.stderr) and result.returncode != 0:
+        return True
+    else:
+        return False
+
+
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.returncode == 0:
+        return True
+    else:
+        return False
